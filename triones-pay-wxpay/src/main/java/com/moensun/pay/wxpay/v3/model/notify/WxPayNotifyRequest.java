26544742463202439793dc3cba1d1e2dea155f26
bane.shi <fengxiaotx@163.com>
2023-11-27 11:45:14 +0800

    feat:

--- triones-pay-wxpay/src/main/java/com/moensun/pay/wxpay/v3/model/notify/WxPayNotifyRequest.java
+++ triones-pay-wxpay/src/main/java/com/moensun/pay/wxpay/v3/model/notify/WxPayNotifyRequest.java
@@ -14,14 +14,14 @@ public class WxPayNotifyRequest {
     private String serial;
     private String nonce;
     private String timestamp;
-    private String signatureType;
+    private String signature;
     private String body;
 
     public RequestParam toRequestParam() {
         return new RequestParam.Builder()
                 .serialNumber(this.getSerial())
                 .nonce(this.getNonce())
-                .signature(this.getSignatureType())
+                .signature(this.getSignature())
                 .timestamp(this.getTimestamp())
                 .body(this.getBody())
                 .build();
