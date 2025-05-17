1a5c2f00a49d8bf29f1abb05f64e789a4fa7eb70
落雁沙 <fengxiaotx@163.com>
2025-05-17 12:24:38 +0800

    feat:

--- triones-payment-wechatpay/src/main/java/com/trionesdev/payment/wechatpay/v3/payment/app/model/WechatPayAppCreateOrderRequest.java
+++ triones-payment-wechatpay/src/main/java/com/trionesdev/payment/wechatpay/v3/payment/app/model/WechatPayAppCreateOrderRequest.java
@@ -5,8 +5,10 @@ import com.trionesdev.payment.wechatpay.v3.model.WechatPayBaseCreateOrderRequest
 import com.trionesdev.payment.wechatpay.v3.payment.WechatPayConfig;
 import com.trionesdev.payment.wechatpay.v3.payment.app.WechatPayAppConvert;
 import com.wechat.pay.java.service.payments.app.model.PrepayRequest;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.experimental.SuperBuilder;
 
@@ -14,6 +16,8 @@ import lombok.experimental.SuperBuilder;
 @Data
 @Accessors(chain = true)
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WechatPayAppCreateOrderRequest extends WechatPayBaseCreateOrderRequest {
     private Payer payer;
     public PrepayRequest toSdkRequest(WechatPayConfig config) {
