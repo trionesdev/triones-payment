6c1f406c87bf0c9e3fd236b976881c1454014b5e
bane.shi <fengxiaotx@163.com>
2025-07-18 14:18:32 +0800

    feat:

--- triones-payment-alipay/src/main/java/com/trionesdev/payment/alipay/v3/payment/ordercode/AlipayOrderCode.java
+++ triones-payment-alipay/src/main/java/com/trionesdev/payment/alipay/v3/payment/ordercode/AlipayOrderCode.java
@@ -5,6 +5,7 @@ import com.alipay.v3.model.AlipayTradePrecreateResponseModel;
 import com.trionesdev.payment.alipay.v3.AlipayConfig;
 import com.trionesdev.payment.alipay.v3.payment.AlipayPaymentCommons;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 
 public class AlipayOrderCode extends AlipayPaymentCommons {
     public AlipayOrderCode(AlipayConfig alipayConfig) {
@@ -13,6 +14,9 @@ public class AlipayOrderCode extends AlipayPaymentCommons {
 
     @SneakyThrows
     public AlipayTradePrecreateResponseModel createOrder(AlipayTradePrecreateModel model) {
+        if (StringUtils.isBlank(model.getProductCode())){
+            model.setProductCode("QR_CODE_OFFLINE");
+        }
         return alipayTradeApi.precreate(model);
     }
 }
