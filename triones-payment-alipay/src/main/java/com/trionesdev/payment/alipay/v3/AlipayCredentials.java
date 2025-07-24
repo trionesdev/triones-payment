aa4105ca2db33f9678482676e89a010c9056a344
落雁沙 <fengxiaotx@163.com>
2025-07-24 21:41:20 +0800

    feat:[]

--- triones-payment-alipay/src/main/java/com/trionesdev/payment/alipay/v3/AlipayCredentials.java
+++ triones-payment-alipay/src/main/java/com/trionesdev/payment/alipay/v3/AlipayCredentials.java
@@ -1,6 +1,11 @@
 package com.trionesdev.payment.alipay.v3;
 
 import com.alipay.v3.util.model.AlipayConfig;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class AlipayCredentials extends AlipayConfig {
+    private String notifyUrl;
 }
